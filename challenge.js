22e1afa62530962558ce2e9c202db8b8e0fc416f
Leah Grim <grimchelate@gmail.com>
2021-12-09 13:07:06 -0600

    final changes

---
+++ challenge.js
@@ -0,0 +1,19 @@
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('in onReady')
+//atleast two types of selectors 
+
+
+//DOM traversal 
+
+//Render HTML w append
+
+//use a getter and use a setter (line 17)
+
+let textHeader = $('#text_lists').append("text here");
+    console.log('text_lists is:', textHeader);
+
+}
+
+
